4e99848dc78a062a7a5c9cc7f69b46c3dab7d41b
craftersmine <rodion.morozov.01@gmail.com>
2022-11-27 01:40:43 +0700

    Added ASAR archive header file metadata collection class

---
+++ craftersmine.Asar.Net/AsarArchiveFileCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace craftersmine.Asar.Net
+{
+    public class AsarArchiveFileCollection : Dictionary<string, AsarArchiveFile>
+    {
+    }
+}
