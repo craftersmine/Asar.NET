5b4499fb779d7a4b859e0e594109cafb44f3e1b7
craftersmine <rodion.morozov.01@gmail.com>
2022-11-27 01:41:13 +0700

    Added ASAR archive class

---
+++ craftersmine.Asar.Net/AsarArchive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace craftersmine.Asar.Net
+{
+    public class AsarArchive
+    {
+        private Stream archiveStream;
+
+        public long HeaderSize { get; private set; }
+        public AsarArchiveFileCollection Files { get; private set; }
+
+        public AsarArchive(Stream stream)
+        {
+            archiveStream = stream;
+        }
+
+        public AsarArchive(string fileName)
+        {
+            archiveStream = File.OpenRead(fileName);
+        }
+    }
+}
