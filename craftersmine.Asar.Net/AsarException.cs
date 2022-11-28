4c22dd7df51c12c211291c771dc9cf99ee28ca77
craftersmine <rodion.morozov.01@gmail.com>
2022-11-28 17:18:39 +0700

    Added Asar Exception class for Asar Archive related exceptions

---
+++ craftersmine.Asar.Net/AsarException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace craftersmine.Asar.Net
+{
+    [Serializable]
+    public class AsarException : Exception
+    {
+        public AsarException()
+        {
+        }
+
+        public AsarException(string message) : base(message)
+        {
+        }
+
+        public AsarException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected AsarException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
