e84d33ce1f4b112b7cf8d7e4237e08f587d93123
craftersmine <rodion.morozov.01@gmail.com>
2022-11-28 19:52:42 +0700

    Implemented basic reads from ASAR archive

--- craftersmine.Asar.Net/AsarArchive.cs
+++ craftersmine.Asar.Net/AsarArchive.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,19 +12,152 @@ namespace craftersmine.Asar.Net
     /// </summary>
     public partial class AsarArchive : IDisposable
     {
+        // represents size of header whole size of ASAR header integer, but since it is 4 bytes long, we can assume that it is ASAR 
+        private static readonly byte[] Signature = new byte[] {0x04, 0x00, 0x00, 0x00};
+
+        private int headerSize;
+        private int headerMetadataSize;
+        private int filesOffset;
+        private byte[] headerData;
         private Stream archiveStream;
+        private BinaryReader archiveBinaryReader;
 
-        public long HeaderSize { get; private set; }
-        public AsarArchiveFileCollection Files { get; private set; }
+        /// <summary>
+        /// Gets a size of the header metadata
+        /// </summary>
+        public long HeaderSize => headerMetadataSize;
+        /// <summary>
+        /// Gets an offset value where in the archive actual archived files data starts
+        /// </summary>
+        public int FilesDataOffset => filesOffset;
+        /// <summary>
+        /// Gets a <see langword="true"/> if archive loaded from file, otherwise <see langword="false"/>
+        /// </summary>
+        public bool IsFile { get; private set; }
+        /// <summary>
+        /// Gets a path to archive file or empty string if archive was loaded from file
+        /// </summary>
+        public string FilePath { get; private set; } = string.Empty;
+        /// <summary>
+        /// Gets a path to unpacked files of this ASAR archive
+        /// </summary>
+        public string UnpackedFilesPath
+        {
+            get
+            {
+                if (!IsFile || string.IsNullOrWhiteSpace(FilePath))
+                    return string.Empty;
 
+                string path = Path.Combine(Path.GetDirectoryName(FilePath),
+                    Path.GetFileNameWithoutExtension(FilePath) + ".asar.unpacked");
+                return path;
+            }
+        }
+        /// <summary>
+        /// Gets packed files metadata, such as file sizes, offsets, integrity data, etc.
+        /// </summary>
+        public AsarArchiveFile Files { get; private set; }
+
+        /// <summary>
+        /// Opens an ASAR archive from data stream
+        /// </summary>
+        /// <param name="stream">Data stream with ASAR archive data</param>
+        /// <exception cref="AsarException">When data in the stream is not ASAR archive</exception>
         public AsarArchive(Stream stream)
         {
             archiveStream = stream;
+            archiveBinaryReader = new BinaryReader(archiveStream);
+
+            byte[] sig = archiveBinaryReader.ReadBytes(4);
+            ReadOnlySpan<byte> sigSpan = new ReadOnlySpan<byte>(sig);
+            if (!sigSpan.SequenceEqual(Signature))
+                throw new AsarException("Not an ASAR archive in file/stream");
+
+            headerSize = (int)archiveBinaryReader.ReadUInt32();
+            archiveStream.Seek(4, SeekOrigin.Current);
+
+            headerMetadataSize = (int)archiveBinaryReader.ReadUInt32();
+            headerData = archiveBinaryReader.ReadBytes(headerMetadataSize);
+            string header = Encoding.UTF8.GetString(headerData);
+
+            filesOffset = headerSize + 8;
+            Files = JsonConvert.DeserializeObject<AsarArchiveFile>(header);
+            Files.IsRoot = true;
+            PopulateParents(Files);
         }
 
-        public AsarArchive(string fileName)
+        /// <summary>
+        /// Opens an ASAR archive from file
+        /// </summary>
+        /// <param name="filePath">Path to ASAR archive</param>
+        /// <exception cref="AsarException">When data in the file is not ASAR archive</exception>
+        public AsarArchive(string filePath) : this(File.OpenRead(filePath))
         {
-            archiveStream = File.OpenRead(fileName);
+            IsFile = true;
+            FilePath = filePath;
+        }
+
+        /// <summary>
+        /// Closes an ASAR archive, closes file stream and disposes all used resources
+        /// </summary>
+        public void Dispose()
+        {
+            archiveBinaryReader.Dispose();
+            archiveStream?.Dispose();
+        }
+
+        public async Task<byte[]> ReadBytesFromFileAsync(string path)
+        {
+            AsarArchiveFile file = FindFile(path);
+            return await ReadBytes(file);
+        }
+
+        public async Task<byte[]> ReadBytes(AsarArchiveFile file)
+        {
+            byte[] data = new byte[file.Size];
+
+            if (file.IsUnpacked)
+            {
+                return await ReadUnpackedBytesAsync(file);
+            }
+
+            archiveStream.Seek(filesOffset + (int)file.Offset, SeekOrigin.Begin);
+            int read = await archiveStream.ReadAsync(data, 0, (int)file.Size);
+
+            return data;
+        }
+
+        public Stream OpenFileAsStream(AsarArchiveFile file)
+        {
+            if (file.IsUnpacked)
+                return OpenUnpackedAsStream(file);
+
+            AsarFileStream asarFileStream =
+                new AsarFileStream(archiveStream, filesOffset, (int) file.Offset, (int) file.Size);
+            return asarFileStream;
+        }
+
+        /// <summary>
+        /// Gets an ASAR archived file by specified path in archive
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public AsarArchiveFile FindFile(string path)
+        {
+            string[] dirs = Path.GetDirectoryName(path)?.Split(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (dirs is null || dirs.Length == 0) return Files.Files.FirstOrDefault(f => f.Key == path).Value;
+
+            AsarArchiveFile lastDir = Files;
+            foreach (var dir in dirs)
+            {
+                lastDir = lastDir.Files[dir];
+            }
+
+            if (!lastDir.Files.ContainsKey(Path.GetFileName(path)))
+                throw new FileNotFoundException("File is not found in ASAR archive", path);
+
+            return lastDir.Files[Path.GetFileName(path)];
         }
     }
 }
