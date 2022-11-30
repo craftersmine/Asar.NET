02fb3862fd77cac1eafcc9231755dc8a79ad219d
craftersmine <rodion.morozov.01@gmail.com>
2022-12-01 02:14:28 +0700

    Updated test class

--- craftersmine.Asar.NET.Tests/AsarArchiveTests.cs
+++ craftersmine.Asar.NET.Tests/AsarArchiveTests.cs
@@ -28,18 +28,22 @@ namespace craftersmine.Asar.NET.Tests
             //byte[] dataFile0 = await archive.ReadBytesAsync("file0.txt");
             //string dataStringFile0 = Encoding.Default.GetString(dataFile0);
 
-            AsarArchivePackerDataBuilder builder = AsarArchivePackerDataBuilder.CreateBuilder("asars\\firstAsar", "testAsarArc");
-            AsarArchivePackerData data = builder
-                .AddDirectory("TestsInput//packthis-unpack.asar.unpacked", true)
-                .AddFile("craftersmine.Asar.Net.dll",
-                    false,
-                    true)
-                .PerformFileSort(true)
-                .CreateArchiveData();
-            AsarArchivePacker packer = new AsarArchivePacker(data);
+            //AsarArchivePackerDataBuilder builder = AsarArchivePackerDataBuilder.CreateBuilder("asars\\firstAsar", "testAsarArc");
+            //AsarArchivePackerData data = builder
+            //    .AddDirectory("TestsInput//packthis-unpack.asar.unpacked", true)
+            //    .AddFile("craftersmine.Asar.Net.dll",
+            //        false,
+            //        true)
+            //    .PerformFileSort(true)
+            //    .CreateArchiveData();
+            //AsarArchivePacker packer = new AsarArchivePacker(data);
 
-            packer.OnAsarArchivePacked += PackerOnOnAsarArchivePacked;
-            await packer.PackAsync();
+            //packer.OnAsarArchivePacked += PackerOnOnAsarArchivePacked;
+            //await packer.PackAsync();
+
+            AsarArchive archive = new AsarArchive("TestsInput\\packthis-unpack.asar");
+            AsarArchiveUnpacker unpacker = new AsarArchiveUnpacker(archive);
+            await unpacker.UnpackAsync("asars\\unpacked-packthis-unpack");
         }
 
         private void PackerOnOnAsarArchivePacked(object? sender, AsarPackingCompletedEventArgs e)
