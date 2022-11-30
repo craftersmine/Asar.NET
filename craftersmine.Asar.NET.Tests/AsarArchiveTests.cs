feef30ef06ccf76ca5476b35a23dd892f42e95a8
craftersmine <rodion.morozov.01@gmail.com>
2022-11-30 21:52:18 +0700

    Updated tests

--- craftersmine.Asar.NET.Tests/AsarArchiveTests.cs
+++ craftersmine.Asar.NET.Tests/AsarArchiveTests.cs
@@ -13,20 +13,39 @@ namespace craftersmine.Asar.NET.Tests
     {
         public TestContext TestContext { get; set; }
 
-        [TestMethod, DeploymentItem("TestsInput\\packthis-unpack.asar")]
+        [TestMethod, DeploymentItem("TestsInput\\packthis-unpack.asar"), DeploymentItem("craftersmine.Asar.Net.dll"), DeploymentItem("TestsInput")]
         public async Task LoadAsarTests()
         {
-            AsarArchive archive = new AsarArchive("TestsInput\\packthis-unpack.asar");
+            //AsarArchive archive = new AsarArchive("TestsInput\\packthis-unpack.asar");
 
-            AsarArchiveFile file = archive.FindFile("dir2/file3.txt");
-            Stream stream = archive.OpenFileAsStream(file);
-            //byte[] data = await archive.ReadBytes(file);
-            byte[] data = new byte[stream.Length];
-            int read = stream.Read(data, 0, data.Length);
-            string dataString = Encoding.Default.GetString(data);
+            //AsarArchiveFile file = archive.FindFile("dir2/file3.txt");
+            //Stream stream = archive.OpenFileAsStream(file);
+            ////byte[] data = await archive.ReadBytes(file);
+            //byte[] data = new byte[stream.Length];
+            //int read = stream.Read(data, 0, data.Length);
+            //string dataString = Encoding.Default.GetString(data);
 
-            byte[] dataFile0 = await archive.ReadBytesAsync("file0.txt");
-            string dataStringFile0 = Encoding.Default.GetString(dataFile0);
+            //byte[] dataFile0 = await archive.ReadBytesAsync("file0.txt");
+            //string dataStringFile0 = Encoding.Default.GetString(dataFile0);
+
+            AsarArchivePackerDataBuilder builder = AsarArchivePackerDataBuilder.CreateBuilder("asars\\firstAsar", "testAsarArc");
+            AsarArchivePackerData data = builder
+                .AddDirectory("TestsInput//packthis-unpack.asar.unpacked", true)
+                .AddFile("craftersmine.Asar.Net.dll",
+                    false,
+                    true)
+                .PerformFileSort(true)
+                .CreateArchiveData();
+            AsarArchivePacker packer = new AsarArchivePacker(data);
+
+            packer.OnAsarArchivePacked += PackerOnOnAsarArchivePacked;
+            await packer.PackAsync();
+        }
+
+        private void PackerOnOnAsarArchivePacked(object? sender, AsarPackingCompletedEventArgs e)
+        {
+            int files = e.PackedArchive.Files.GetFileCount();
+            Assert.IsNotNull(e.PackedArchive);
         }
     }
 }
