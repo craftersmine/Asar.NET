b7dfab7253ae8dccb0c486a2d655b1bcf57bc2f7
craftersmine <rodion.morozov.01@gmail.com>
2022-11-30 21:52:08 +0700

    Added extensions and XML docs

--- craftersmine.Asar.Net/AsarArchiveFileCollection.cs
+++ craftersmine.Asar.Net/AsarArchiveFileCollection.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace craftersmine.Asar.Net
 {
+    /// <summary>
+    /// Represents an ASAR archive file collection
+    /// </summary>
     public class AsarArchiveFileCollection : Dictionary<string, AsarArchiveFile>
     {
+    }
+
+    /// <summary>
+    /// Contains static extension methods for <see cref="AsarArchiveFileCollection"/>
+    /// </summary>
+    public static class AsarArchiveFileCollectionExtensions
+    {
+        /// <summary>
+        /// Converts sorted dictionary to <see cref="AsarArchiveFileCollection"/>
+        /// </summary>
+        /// <param name="dictionary">Sorted dictionary</param>
+        /// <returns><see cref="AsarArchiveFileCollection"/> with sorted data</returns>
+        public static AsarArchiveFileCollection ToAsarArchiveFileCollection(
+            this SortedDictionary<string, AsarArchiveFile> dictionary)
+        {
+            AsarArchiveFileCollection collection = new AsarArchiveFileCollection();
+            foreach (KeyValuePair<string, AsarArchiveFile> kvp in dictionary)
+            {
+                collection.Add(kvp.Key, kvp.Value);
+            }
+
+            return collection;
+        }
     }
 }
