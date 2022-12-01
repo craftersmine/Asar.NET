550c6457caaa36664b33d69c4995a9819c5a311b
craftersmine <rodion.morozov.01@gmail.com>
2022-12-01 20:55:40 +0700

    Removed unnecessary using

--- craftersmine.Asar.Net/AsarArchiveFile.cs
+++ craftersmine.Asar.Net/AsarArchiveFile.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
