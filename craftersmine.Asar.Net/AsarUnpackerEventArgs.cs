96e81f69b849859c957ceebfad523ba40c216704
craftersmine <rodion.morozov.01@gmail.com>
2022-12-01 19:00:51 +0700

    Added ASAR unpacker event args class

---
+++ craftersmine.Asar.Net/AsarUnpackerEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace craftersmine.Asar.Net
+{
+    public class AsarUnpackingStatusChangedEventArgs : EventArgs
+    {
+        public int TotalFiles { get; private set; }
+        public int CurrentFile { get; private set; }
+        public string OutputFilePath { get; private set; }
+        public AsarArchiveFile CurrentFileData { get; private set; }
+        //public AsarUnpackingStatus Status { get; private set; }
+
+        internal AsarUnpackingStatusChangedEventArgs(int totalFiles, int currentFile, string outputFilePath, AsarArchiveFile currentFileData)
+        {
+            TotalFiles = totalFiles;
+            CurrentFile = currentFile;
+            OutputFilePath = outputFilePath;
+            CurrentFileData = currentFileData;
+        }
+    }
+
+    public class AsarUnpackingCompletedEventArgs : EventArgs
+    {
+        public string OutputDirectoryPath { get; private set; }
+        public DirectoryInfo OutputDirectory { get; private set; }
+
+        internal AsarUnpackingCompletedEventArgs(string outputDirectoryPath, DirectoryInfo outputDirectory)
+        {
+            OutputDirectoryPath = outputDirectoryPath;
+            OutputDirectory = outputDirectory;
+        }
+    }
+
+    //public enum AsarUnpackingStatus
+    //{
+
+    //}
+}
